ba3ad946c6897a22c23fa80f13cf9d6703a6063c
Nika Yakubova <lilit666an@gmail.com>
2020-12-01 13:07:32 +0300

    ADD: cross-env, optimization fot html/css/js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,29 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin  = require('copy-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
 
 
 const isDev = process.env.NODE_ENV === 'development'
+const isProd= !isDev
+
+const optimization = ()  => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+    if(isProd){
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config
+}
+
 
 module.exports  = {
     context: path.resolve(__dirname, 'src'),
@@ -16,11 +36,7 @@ module.exports  = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },  
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
+    optimization: optimization(),
     devServer: {
         hot: isDev,
         open: true,
